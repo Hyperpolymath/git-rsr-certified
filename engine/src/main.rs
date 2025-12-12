c8ccf8579d51387a7be48d8b924d5c5a1a44c529
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2025-12-12 16:43:17 +0000

    chore: Upgrade HTTP → HTTPS URLs (RSR security)

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -276,7 +276,7 @@ fn generate_badge_svg(tier: &CertificationTier, _style: &str) -> String {
     let label = tier.code();
 
     format!(
-        r##"<svg xmlns="http://www.w3.org/2000/svg" width="120" height="20">
+        r##"<svg xmlns="https://www.w3.org/2000/svg" width="120" height="20">
 <linearGradient id="b" x2="0" y2="100%">
 <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
 <stop offset="1" stop-opacity=".1"/>
